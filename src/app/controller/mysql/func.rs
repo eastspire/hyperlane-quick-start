4f9dad62aa24167a2e7e58f7e7589aeacac1701c
尤雨东 <root@ltpp.vip>
2025-02-23 21:06:53 +0800

    feat: hyperlane

--- src/app/controller/mysql/func.rs
+++ src/app/controller/mysql/func.rs
@@ -2,9 +2,10 @@ use crate::{plugin::mysql::func::insert_record, *};
 
 pub async fn insert(arc_lock_controller_data: ArcRwLockControllerData) {
     insert_record().await;
-    let send_res: ResponseResult =
-        send_response(&arc_lock_controller_data, 200, "hello hyperlane => /").await;
-    let controller_data: ControllerData = get_controller_data(&arc_lock_controller_data).await;
+    let send_res: ResponseResult = arc_lock_controller_data
+        .send_response(200, "hello hyperlane => /")
+        .await;
+    let controller_data: ControllerData = arc_lock_controller_data.get_controller_data().await;
     controller_data.get_log().info(
         format!("Response result => {:?}", send_res),
         log_debug_handler,
