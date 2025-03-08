c3c75430c2dd4cac4f70ef5427f1537299690c96
尤雨东 <root@ltpp.vip>
2025-03-08 19:08:21 +0800

    add sse

--- src/app/controller/root/func.rs
+++ src/app/controller/root/func.rs
@@ -1,7 +1,18 @@
 use crate::*;
+use hyperlane::tokio::time::sleep;
+use std::time::Duration;
 
 pub async fn root(controller_data: ControllerData) {
     let _ = controller_data
-        .send_response(200, "Hello hyperlane => /")
+        .set_response_header(CONTENT_TYPE, TEXT_EVENT_STREAM)
+        .await
+        .send_response(200, "")
         .await;
+    for i in 0..10 {
+        let _ = controller_data
+            .send_response_body(format!("data: {}{}", i, HTTP_DOUBLE_BR))
+            .await;
+        sleep(Duration::from_secs(1)).await;
+    }
+    let _ = controller_data.close().await;
 }
