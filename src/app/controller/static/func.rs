c5d68dec9f7b2980f98e1151bff74246cfda256e
尤雨东 <root@ltpp.vip>
2025-04-14 13:36:38 +0800

    fix: content-type

--- src/app/controller/static/func.rs
+++ src/app/controller/static/func.rs
@@ -12,7 +12,6 @@ pub async fn handle(ctx: Context) {
     let extension_name: String = FileExtension::get_extension_name(&decode_file);
     let content_type: &str = FileExtension::parse(&extension_name).get_content_type();
     let data: Vec<u8> = async_read_from_file(&path).await.unwrap_or_default();
-    let content_type: String = content_type_charset(content_type, UTF8);
     ctx.set_response_header(CONTENT_TYPE, content_type_charset(TEXT_HTML, UTF8))
         .await
         .set_response_header(CONTENT_TYPE, content_type)
