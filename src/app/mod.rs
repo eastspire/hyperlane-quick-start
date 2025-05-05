830bb4b7b4e96de684fe1758d4cc88e3ac7e9bbd
尤雨东 <root@ltpp.vip>
2025-05-05 08:36:48 +0800

    feat: fir

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,6 +1,8 @@
+pub(crate) mod aspect;
 pub(crate) mod controller;
 pub(crate) mod dao;
 pub(crate) mod exception;
+pub(crate) mod filter;
 pub(crate) mod interrupt;
 pub(crate) mod mapper;
 pub(crate) mod middleware;
--- src/app/model/mod.rs
+++ src/app/model/mod.rs
@@ -3,6 +3,7 @@ pub(crate) mod bo;
 pub(crate) mod domain;
 pub(crate) mod dto;
 pub(crate) mod entity;
+pub(crate) mod param;
 pub(crate) mod po;
 pub(crate) mod pojo;
 pub(crate) mod vo;
