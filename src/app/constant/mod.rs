7618f07158e4522723127fc185d15395d1c7fba0
尤雨东 <root@ltpp.vip>
2025-05-05 09:20:07 +0800

    feat: dir

--- src/app/constant/mod.rs
+++
@@ -1,2 +0,0 @@
-pub(crate) mod consist;
-pub(crate) mod enums;
--- src/app/model/mod.rs
+++ src/app/model/mod.rs
@@ -2,11 +2,11 @@ pub(crate) mod application;
 pub(crate) mod bean;
 pub(crate) mod business;
 pub(crate) mod data;
+pub(crate) mod data_access;
 pub(crate) mod data_transfer;
 pub(crate) mod domain;
-pub(crate) mod entity;
 pub(crate) mod param;
-pub(crate) mod po;
+pub(crate) mod persistent;
 pub(crate) mod view;
 
 pub(super) use super::*;
